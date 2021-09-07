c375877fff5fd431ad9b0b3186921fac6b6909c7
Samson Kwaku Nkrumah <samsonnkrumah253@gmail.com>
2021-09-07 19:16:56 +0530

    Countriies controller done

---
+++ controllers/countries.js
@@ -0,0 +1,13 @@
+const countriesData = require("../data/countries.json");
+
+const getAllCountries = (req, res, next) => {
+  const countries = countriesData.countries.map((data) => data.name);
+  res.send(countries);
+};
+
+const getAllCountriesAndCode = (req, res, next) => {
+  const countries = countriesData.countries;
+  res.send(countries);
+};
+
+module.exports = { getAllCountries, getAllCountriesAndCode };
