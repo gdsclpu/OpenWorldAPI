00b736ab787999bdce670ca4d79b16fa88568e96
Samson Kwaku Nkrumah <samsonnkrumah253@gmail.com>
2021-10-03 23:03:08 +0530

    Routes of cities added

--- routes/cities.js
+++ routes/cities.js
@@ -1,4 +1,15 @@
 const router = require("express").Router();
 const controller = require("../controllers/cities");
 
+router.get(
+  "/get/all/cities/country/:country",
+  controller.getAllCitiesOfCountry
+);
+
+router.get("/get/all/cities/state/:country", controller.getAllCitiesOfState);
+router.get(
+  "/get/all/cities/district/:country",
+  controller.getAllCitiesOfDistrict
+);
+
 module.exports = router;
