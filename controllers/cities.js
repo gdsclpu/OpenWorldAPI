4b1a682be393918ba86e378ec0a3fbf2092f8365
Samson Kwaku Nkrumah <samsonnkrumah253@gmail.com>
2021-10-03 23:52:25 +0530

    All cities of state done

--- controllers/cities.js
+++ controllers/cities.js
@@ -1,3 +1,4 @@
+const { allCitiesOfCountry, allCitiesOfState } = require("../custom/custom");
 let { GHANA, INDIA } = require("../data/actions");
 const GhanaData = require("../data/countries/ghana.json");
 const IndiaData = require("../data/countries/india.json");
@@ -13,12 +14,7 @@ const getAllCitiesOfCountry = async (req, res, next) => {
   reverse = reverse ? (reverse === "false" ? false : true) : true;
   switch (country.toUpperCase()) {
     case GHANA:
-      let cities = GhanaData.states.map((data) =>
-        data.districts.map((datas) => datas.cities)
-      );
-      //   res
-      //     .status(200)
-      //     .send({ districts: allDistrictInCountry(GhanaData, reverse) });
+      res.status(200).send({ cities: allCitiesOfCountry(GhanaData, reverse) });
       return;
 
     case INDIA:
@@ -30,7 +26,38 @@ const getAllCitiesOfCountry = async (req, res, next) => {
   }
 };
 
-const getAllCitiesOfState = async (req, res, next) => {};
+const getAllCitiesOfState = async (req, res, next) => {
+  let { country } = req.params;
+  let { reverse, state } = req.query;
+
+  if (!country || !state) {
+    res.status(400).send({ message: "Please provide both country and state" });
+  }
+
+  reverse = reverse ? (reverse === "false" ? false : true) : true;
+  switch (country.toUpperCase()) {
+    case GHANA:
+      let index = GhanaData.states.findIndex(
+        (data) => data.name.toLowerCase() === state.toLowerCase()
+      );
+
+      if (index === -1) {
+        res.status(400).send({ message: "State not found" });
+      }
+      res
+        .status(200)
+        .send({ cities: allCitiesOfState(GhanaData, index, reverse) });
+      return;
+
+    case INDIA:
+      res.status(200).send({ districts: cities });
+      return;
+
+    default:
+      res.send("Nothingness");
+  }
+  //   res.send(cities);
+};
 
 const getAllCitiesOfDistrict = async (req, res, next) => {};
 
--- custom/custom.js
+++ custom/custom.js
@@ -45,6 +45,40 @@ const allDistrictOfState = (countryData, index, order) => {
 // --------------------------------------------------------------------
 
 // -------------------CITIES-------------------------------------------
+const allCitiesOfCountry = (countryData, order) => {
+  let cities = countryData.states.map((data) =>
+    data.districts.map((datas) => datas.cities)
+  );
+  let newData = [];
+  for (let i = 0; i < cities.length; i++) {
+    for (let j = 0; j < cities[i].length; j++) {
+      newData = newData.concat(cities[i][j]);
+    }
+  }
+  return order ? newData.sort() : newData.reverse();
+};
+
+const allCitiesOfState = (countryData, index, order) => {
+  return order
+    ? []
+        .concat(
+          ...countryData.states[index].districts.map((data) => [...data.cities])
+        )
+        .sort()
+        .filter((data) => data !== "")
+    : []
+        .concat(
+          ...countryData.states[index].districts.map((data) => [...data.cities])
+        )
+        .reverse()
+        .filter((data) => data !== "");
+};
 // --------------------------------------------------------------------
 
-module.exports = { allStates, allDistrictInCountry, allDistrictOfState };
+module.exports = {
+  allStates,
+  allDistrictInCountry,
+  allDistrictOfState,
+  allCitiesOfCountry,
+  allCitiesOfState,
+};
