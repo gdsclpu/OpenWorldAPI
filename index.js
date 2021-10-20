16670b5faf32bc8d77bf57d52e756e11a7f21e76
Samson Kwaku Nkrumah <samsonnkrumah253@gmail.com>
2021-10-20 14:55:36 +0530

    Swagger code added

--- index.js
+++ index.js
@@ -5,12 +5,15 @@ const dotenv = require("dotenv");
 const hpp = require("hpp");
 const path = require("path");
 const helmet = require("helmet");
+const swaggerUi = require("swagger-ui-express");
+const swaggerJsdoc = require("swagger-jsdoc");
 
 // Routes
 const CountriesRoutes = require("./routes/countries");
 const StatesRoutes = require("./routes/states");
 const DistrictRoutes = require("./routes/districts");
 const CitiesRoute = require("./routes/cities");
+const { options } = require("./apiDocs");
 
 // Subscribing to unhandledRejections
 process.on("unhandledRejection", (err) => {
@@ -22,6 +25,8 @@ process.on("unhandledRejection", (err) => {
 dotenv.config();
 
 const app = express();
+
+const specs = swaggerJsdoc(options);
 
 // Middlewares
 // Defining template
@@ -39,6 +44,7 @@ app.use(express.static(path.join(__dirname, "public")));
 const port = process.env.PORT || 3000;
 
 // Routes
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 app.use("/countries", CountriesRoutes);
 app.use("/states", StatesRoutes);
 app.use("/district", DistrictRoutes);
